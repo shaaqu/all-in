263966308abf0b637c043a429c3d444a3d70d3b0
tuswoj <w.m.lukasik@gmail.com>
2025-01-16 21:38:42 +0100

    Create webstorm code styles

--- backend/src/controllers/helloWorldController.ts
+++ backend/src/controllers/helloWorldController.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
 
 export function getHello( req: Request, res: Response ): void {
-    res.send("Hello World");
+    res.send( "Hello World" );
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 3000
 
 app.listen();
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-});
+app.listen( PORT, () => {
+    console.log( `Server is running on http://localhost:${ PORT }` )
+} );
